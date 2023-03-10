04c16b3d74887f8d4a5b8faedfbd02c8651e03d5
“Zheng <jamestang1030@outlook.com>
2023-03-10 07:52:38 -0700

    test2

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -50,17 +50,6 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void addCity(City city){
-
+        cities.add(city);
     }
-
-    private CustomList list;
-    /**
-     * create a mocklist for my citylist
-     * @return
-     */
-    public CustomList MockCityList(){
-        list = new CustomList(null,new ArrayList<>());
-        return list;
-    }
-
 }
